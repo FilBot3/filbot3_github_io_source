c6c67f87d301d61c08f7db8ffa5640e847dcb934
Phillip Dudley <Predatorian3@gmail.com>
2020-05-06 21:44:28 -0500

    Updated references in repo parent files.

--- Makefile
+++ Makefile
@@ -1,22 +1,22 @@
 setup:
-	git clone https://github.com/predatorian3/predatorian3.github.io
+	git clone https://github.com/filbot3/filbot3.github.io
 
 python:
 	python -m pip install -r python-3-requirements.txt
-	
+
 serve:
 	python -m mkdocs serve
-	
+
 gh-pages:
 	python -m mkdocs build
 	# Because GitHub is dumb.
 	# https://www.mkdocs.org/user-guide/deploying-your-docs/#organization-and-user-pages
 	# Seems that SSH Keys that have passwords on them, unless you're using an
 	# ssh-agent seem to fail with the mkdocs gh-deploy command. So, that is dumb.
-	cd ../predatorian3.github.io/
+	cd ../filbot3.github.io/
 	#python -m mkdocs gh-deploy \
 	#  --config-file ../predatorian3_github_io_source/mkdocs.yml --remote-name master
-	cp -R ../predatorian3_github_io_source/site/* ./
+	cp -R ../filbot3_github_io_source/site/* ./
 	git add .
 	git commit -m 'Built on $(shell date +"%y-%m-%d-%k%M%S")'
 	git push origin master
--- shovel/shovel.py
+++ shovel/shovel.py
@@ -8,26 +8,26 @@ from shovel import task
 def setup():
     """ Setup the User's GitHub Pages repo.
     """
-    os.system("""git clone https://github.com/predatorian3/predatorian3.github.io""")
+    os.system("""git clone https://github.com/filbot3/filbot3.github.io""")
 
 @task
 def serve():
     """ MkDocs Serve
     """
     os.system("""python -m mkdocs serve""")
-    
+
 @task
 def ghpages():
     """ MkDocs gh-deploy
     Because GitHub is stupid.
     https://www.mkdocs.org/user-guide/deploying-your-docs/#organization-and-user-pages
-    
+
     Seems you'll need to have an SSH Key with either no password, or loaded into
     an ssh-agent of some sort.
     """
     import datetime
     os.system("""python -m mkdocs build""")
-    os.chdir("../predatorian3.github.io/")
+    os.chdir("../filbot3.github.io/")
     os.system("""git add .""")
     right_now = datetime.date.today().ctime()
     os.system(f"""git commit -m 'Built on {right_now}'""")
